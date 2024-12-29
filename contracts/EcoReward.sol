bd69e95e6ba8c75f02ae39fca54bcf8fa188a369
iuliana14 <iuliana.procop@yahoo.com>
2024-12-29 17:14:24 +0200

    Changed contract

--- contracts/EcoReward.sol
+++ contracts/EcoReward.sol
@@ -8,8 +8,6 @@ contract EcoReward {
     event RewardIssued(address indexed user, uint amount);
     event RewardClaimed(address indexed user, uint amount);
     event ActivityAdded(string activityName, uint rewardRate);
-    event FundsDeposited(address indexed user, uint amount);
-    event ContractFundsUpdated(uint newBalance);
 
     struct Activity {
         string name;
@@ -31,13 +29,11 @@ contract EcoReward {
         _;
     }
 
-    // Permite adminului să adauge activități
     function addActivity(string memory activityName, uint rewardRate) public onlyAdmin {
         activities.push(Activity(activityName, rewardRate));
         emit ActivityAdded(activityName, rewardRate);
     }
 
-    // Permite utilizatorului să logheze activitățile și să primească recompense
     function logEcoActivity(uint activityIndex, uint detail, address user) public {
         require(activityIndex < activities.length, "Invalid activity index");
         uint rewardAmount = activities[activityIndex].rewardRate * detail;
@@ -45,7 +41,6 @@ contract EcoReward {
         emit RewardIssued(user, rewardAmount);
     }
 
-    // Permite utilizatorilor să revendice recompensele lor
     function claimReward() public payable {
         uint rewardAmount = rewards[msg.sender];
         require(rewardAmount > 0, "No reward available");
@@ -60,23 +55,11 @@ contract EcoReward {
         emit RewardClaimed(msg.sender, rewardAmount);
     }
 
-    // Permite adminului să adauge fonduri contractului
-    function depositFunds() public payable onlyAdmin {
-        emit ContractFundsUpdated(address(this).balance);
-    }
-
-    // Permite utilizatorilor să adauge fonduri pentru a revendica recompense
-    function depositUserFunds() public payable {
-        emit FundsDeposited(msg.sender, msg.value);
-    }
-
-    // Funcție pentru a obține lista de activități
     function getActivities() public view returns (Activity[] memory) {
-        return activities;
+		return activities;
     }
 
-    // Funcție pentru a verifica balanta contractului
-    function getContractBalance() public view returns (uint) {
-        return address(this).balance;
-    }
+	  // Funcție pentru a permite proprietarului să alimenteze contractul
+    function depositFunds() public payable {}
+
 }
